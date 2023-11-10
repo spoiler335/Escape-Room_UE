c6766119a4c6c54f0ce55928edd4fe0cbd4c796a
Spoiler335 <sushantsai8@gmail.com>
2023-11-10 22:45:02 +0530

    Add Ureal Project files

---
+++ Source/Escape_Room/Escape_Room.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Escape_Room.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape_Room, "Escape_Room" );
---
+++ Source/Escape_Room/Escape_RoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Escape_RoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_ROOM_API AEscape_RoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
